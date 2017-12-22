9be3feefbed7f27b8b96c42832d126d8b5a4777d
lj-pc <228288621@qq.com>
2017-12-22 17:27:46 +0800

    Initial commit ssm + redis + upload html files

---
+++ src/main/java/blogDemo/dao/UserDao.java
@@ -0,0 +1,8 @@
+package blogDemo.dao;
+
+import blogDemo.model.model.user;
+
+public interface UserDao {
+
+    user selectUser();
+}
---
+++ src/main/java/blogDemo/utils/JedisUtil.java
@@ -0,0 +1,53 @@
+package blogDemo.utils;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class JedisUtil {  private static String ADDR = "123.207.250.128";
+    private static int PORT = 6379;
+    private static String AUTH = "liujun";
+
+
+    private static int MAX_IDLE = 200;
+
+    private static int MAX_WAIT = 10000;
+
+    private static int TIMEOUT = 10000;
+
+    private static boolean TEST_ON_BORROW = true;
+
+    private static JedisPool jedisPool = null;
+
+    static {
+        try{
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxIdle(MAX_IDLE);
+            config.setMaxWaitMillis(MAX_WAIT);
+            config.setTestOnBorrow(TEST_ON_BORROW);
+            jedisPool = new JedisPool(config,ADDR,PORT,TIMEOUT,AUTH);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public synchronized static Jedis getJedis(){
+        try{
+            if(jedisPool != null){
+                Jedis jedis = jedisPool.getResource();
+                return jedis;
+            }else{
+                return null;
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void returnResource(final Jedis jedis){
+        if(jedis != null){
+            jedisPool.returnResource(jedis);
+        }
+    }
+}
